1bb8bd91f5431cb42ddb095853f37c8f65d30830
Dave Rapmund <daverapmund@gmail.com>
2019-10-14 14:42:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,21 +9,21 @@ window.addEventListener("DOMContentLoaded", function(){
   document.querySelector('#mouse_container').classList.add('faded-in');
 });
 
-var test = document.querySelectorAll(".temp");
-for(var i = 0; i < test.length; i++){
-  test[i].addEventListener('click', function(e){
+const skillArray = document.querySelectorAll(".skill");
+for(let i = 0; i < skillArray.length; i++){
+  skillArray[i].addEventListener('click', function(e){
     e.preventDefault();
-    var currentClass = e.currentTarget.classList[1];
+    let currentClass = e.currentTarget.classList[1];
     loadData(currentClass);
-    for(var j = 0; j < test.length; j++){
-      test[j].querySelector('svg').classList.add("hide-me");
+    for(let j = 0; j < skillArray.length; j++){
+      skillArray[j].querySelector('svg').classList.add("hide-me");
     }
     e.currentTarget.querySelector('svg').classList.toggle("hide-me");
   })
 }
 
 function loadData(currentClass = "html5"){
-  var request;
+  let request;
   if(window.XMLHttpRequest){
     request = new XMLHttpRequest();
   }else{
@@ -33,7 +33,7 @@ function loadData(currentClass = "html5"){
   request.open('GET', '/js/data.json', true);
   request.onreadystatechange = function(){
     if(request.readyState === 4 && request.status === 200){
-      var clean = JSON.parse(request.responseText);
+      let clean = JSON.parse(request.responseText);
       document.querySelector('.skill-header').innerHTML = clean["text-data"][currentClass].title;
       document.querySelector('.skill-body').innerHTML = clean["text-data"][currentClass].text.join('\n');
     }
